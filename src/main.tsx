43ff2df2d0542fb46a6e6c597185132b0d394902
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-11-13 17:37:03 +0700

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './main.css'
 import App from './App'
-import RegisterForm from './RegisterForm'
+
 
 liff
   .init({ liffId: import.meta.env.VITE_LIFF_ID || '' })
@@ -11,7 +11,7 @@ liff
     ReactDOM.render(
       <React.StrictMode>
         <App />
-        <RegisterForm />
+      
       </React.StrictMode>,
       document.getElementById('root')
     )
