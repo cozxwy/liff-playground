1587b62beab254890dc08fef96cfb597a4578163
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-11-13 21:56:10 +0700

    Update Input2.tsx

--- src/components/Input2.tsx
+++ src/components/Input2.tsx
@@ -20,7 +20,7 @@ export default function Input({
     <>
       {label && <div className={styles.label}>{label}</div>}
       <div className={styles.frame}>
-        <input autoComplete='off' className={styles.input} readOnly={readonly} placeholder={placeHolder} name={name} />
+        <input required autoComplete='off' className={styles.input} readOnly={readonly} placeholder={placeHolder} name={name} />
       </div>
       {helpText && <div className={styles.helpText}>{helpText}</div>}
     </>
