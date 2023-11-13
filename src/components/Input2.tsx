f16700bbf3f4fde4bf380b60f08596231adce938
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-11-13 18:14:07 +0700

    Add files via upload

---
+++ src/components/Input2.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './Input.module.css'
+
+interface InputProps {
+  label?: string
+  helpText?: string
+  readonly?: boolean
+  placeHolder : string
+  name: string
+}
+
+export default function Input({
+  label,
+  helpText,
+  readonly,
+  placeHolder,
+  name,
+}: InputProps) {
+  return (
+    <>
+      {label && <div className={styles.label}>{label}</div>}
+      <div className={styles.frame}>
+        <input autoComplete='off' className={styles.input} readOnly={readonly} placeholder={placeHolder} name={name} />
+      </div>
+      {helpText && <div className={styles.helpText}>{helpText}</div>}
+    </>
+  )
+}
