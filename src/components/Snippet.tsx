4971f9902d362cab9ca6388cf95ee2e73e24899a
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-11-13 18:24:08 +0700

    Add files via upload

--- src/components/Snippet.tsx
+++ src/components/Snippet.tsx
@@ -77,11 +77,7 @@ export default function Snippet({
           && <div className={styles.snippet}>
           <div className={styles.head}>
             <h2 className={styles.title}>
-              <span className={styles.text}>{apiName}</span>
-              <Tag>≥{version}</Tag>
-              {loginRequired && <Tag backgroundColor={primaryRed}>Login Required</Tag>} {inClientOnly && <Tag backgroundColor={primaryRed}>LINE Client only</Tag>}
-              {isInLIFF && <Tag backgroundColor={primaryBlue}>LIFF</Tag>}
-              {isInMINI && <Tag backgroundColor={primaryOrange}>MINI</Tag>}
+           
             </h2>
             <div className={styles.action}>
               <Button
